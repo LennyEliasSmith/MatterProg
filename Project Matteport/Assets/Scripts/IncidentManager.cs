9d12a68c2491660f9a40a8b287f36f5e18f42f08
LennyEliasSmith <46759225+LennyEliasSmith@users.noreply.github.com>
2020-05-15 13:10:26 +0300

    ADD

---
+++ Project Matteport/Assets/Scripts/IncidentManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncidentManager : MonoBehaviour
+{
+
+    public static bool isManagerOpen = false;
+
+    public GameObject incidentManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isManagerOpen) {
+                Deactivate();
+            }
+            else
+            {
+                 Activate();
+            }
+        }
+        void Deactivate()
+        {
+
+            incidentManager.SetActive(false);
+            Time.timeScale = 1f;
+            isManagerOpen = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void Activate()
+        {
+            incidentManager.SetActive(true);
+            Time.timeScale = 0f;
+            isManagerOpen = true;
+
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+
+
+
+    }
+
+    
+      
+
+    
+}
---
+++ Project Matteport/Assets/Scripts/carIncident.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carIncident : MonoBehaviour
+{
+   public GameObject directionalLightObject;
+   private Light directionalLight;
+   Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        directionalLight = directionalLightObject.GetComponent<Light>();
+    }
+
+    //Car Accident Script
+    public void CarAccident()
+    {
+
+        anim.SetBool("isCarActivated", true);
+        Debug.Log("activated");
+
+    }
+
+    public void CarAccidentAftermath()
+    {
+        Debug.Log("it crashed");
+        directionalLight.color = Color.red;
+    }
+}

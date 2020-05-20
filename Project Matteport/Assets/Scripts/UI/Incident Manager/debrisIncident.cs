87c6badcccc2bb1d88e843895fe918cf37ab909f
LennyEliasSmith <46759225+LennyEliasSmith@users.noreply.github.com>
2020-05-20 14:19:08 +0300

    debris

---
+++ Project Matteport/Assets/Scripts/UI/Incident Manager/debrisIncident.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debrisIncident : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DebrisAccident()
+    {
+        anim.SetBool("isDebrisActivated", true);
+        Debug.Log("activated");
+
+
+    }
+}
